96cceb9666f6f1637d53b56c3e1c9cd4d181dcbb
dreik84 <dreik84@mail.ru>
2025-07-07 15:57:57 +0300

    Update linkList2.java

--- linkList2.java
+++ linkList2.java
@@ -1,6 +1,6 @@
 // Работа со связным списком
 
-class LinkListApp {
+class LinkListApp2 {
 	
 	public static void main(String... args) {
 		LinkList theList = new LinkList();
@@ -12,12 +12,16 @@ class LinkListApp {
 
 		theList.displayList();
 
-		while (!theList.isEmpty()) {
-			Link aLink = theList.deleteFirst();
-			System.out.print("Deleted ");
-			aLink.displayLink();
-			System.out.println();
-		}		
+		Link f = theList.find(44);
+
+		if (f != null) System.out.println("Found link with key " + f.iData);
+		else System.out.println("Can't find link");
+
+		Link d = theList.delete(66);
+
+		if (d != null) System.out.println("Deleted link with key " + d.iData);
+		else System.out.println("Can't delete link");
+
 		theList.displayList();
 	}
 }
@@ -54,10 +58,35 @@ class LinkList {
 		first = newLink;
 	}
 
-	public Link deleteFirst() {
-		Link temp = first;
-		first = first.next;
-		return temp;
+	public Link find(int key) {
+		Link current = first;
+
+		while (current.iData != key) {
+			
+			if (current.next == null) return null;
+			else current = current.next;
+		}
+
+		return current;
+	}
+
+	public Link delete(int key) {
+		Link current = first;
+		Link previous = first;
+		
+		while (current.iData != key) {
+			
+			if (current.next == null) return null;
+			else {
+				previous = current;
+				current = current.next;
+			}
+		}
+
+		if (current == first) first = first.next;
+		else previous.next = current.next;
+
+		return current;
 	}
 
 	public void displayList() {
