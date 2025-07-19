71d86bf514f74dd3c5d3154a844810d345c13492
dreik84 <dreik84@mail.ru>
2025-07-19 16:30:12 +0300

    Update interIterator.java

--- interIterator.java
+++ interIterator.java
@@ -1,6 +1,86 @@
 // Использование итератора связанного списка
+import java.io.*;
 
+class InterIterApp {
+	
+	public static void main(String[] args) throws IOException {
+		
+		LinkList theList = new LinkList();
+		ListIterator iter1 = theList.getIterator();
+		long value;
 
+		iter1.insertAfter(20);		
+		iter1.insertAfter(40);		
+		iter1.insertAfter(80);		
+		iter1.insertAfter(60);
+
+		while (true) {
+			
+			System.out.print("Enter first letter of show, reset, ");
+			System.out.print("next, get, before, after, delete: ");
+			System.out.flush();
+			int choice = getChar();
+
+			switch (choice) {
+				case 's':
+					if (!theList.isEmpty()) theList.displayList();
+					else System.out.println("List is empty");
+					break;
+				case 'r':
+					iter1.reset();
+					break;
+				case 'n':
+				      if (!theList.isEmpty() && !iter1.atEnd())
+						iter1.nextLink();
+				      else System.out.println("Can't go to next link");
+				      break;
+				case 'g':
+				      	if (!theList.isEmpty()) {
+					       value = iter1.getCurrent().dData;
+					       System.out.println("Returned " + value);
+					} else System.out.println("List is empty");
+					break;
+				case 'b':
+					System.out.print("Enter value to insert");
+					System.out.flush();
+					value = getInt();
+					iter1.insertBefore(value);
+					break;
+				case 'a':
+					System.out.print("Enter value to insert");
+                                        System.out.flush();
+                                        value = getInt();
+                                        iter1.insertAfter(value);
+                                        break;
+				case 'd':
+					if (!theList.isEmpty()) {
+						value = iter1.deleteCurrent();
+						System.out.println("Deleted " + value);
+					} else System.out.println("Can't delete");
+					break;
+				default:
+					System.out.println("Invalid entry");
+			}
+		}		
+	}
+
+	public static String getString() throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		String s = br.readLine();
+		return s;
+	}
+
+	public static char getChar() throws IOException {
+		String s = getString();
+		return s.charAt(0);
+	}
+
+	public static int getInt() throws IOException {
+		String s = getString();
+		return Integer.parseInt(s);
+	}
+}
 
 class Link {
 	
@@ -24,7 +104,7 @@ class LinkList {
 
 	public boolean isEmpty() { return first == null; }
 
-	public ListIterator() { return new ListIterator(this); }
+	public ListIterator getIterator() { return new ListIterator(this); }
 
 	public void displayList() {
 		Link currnet = first;
@@ -89,7 +169,21 @@ class ListIterator {
 		}
 	}
 
+	public long deleteCurrent() {
+		long value = current.dData;
 
+		if (previous == null) {
+			ourList.setFirst(current.next);
+			reset();
+		} else {
+			previous.next = current.next;
+			
+			if (atEnd) reset();
+			else current = current.next;
+		}
+
+		return value;
+	}
 }
 
 
