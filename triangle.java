faf37d285d3db42eb2fc30dd7f7dbd7bb6fb1492
dreik84 <dreik84@mail.ru>
2025-07-29 16:07:26 +0300

    Update triangle.java, add diagnostic

--- triangle.java
+++ triangle.java
@@ -15,8 +15,16 @@ class TriangleApp {
 
 	public static int triangle(int n) {
 
-		if (n == 1) return 1;
-		else return (n + triangle(n - 1));
+		System.out.println("Entering: n=" + n);
+
+		if (n == 1) {
+			System.out.println("Returning 1");
+			return 1;
+		} else {
+			int temp = n + triangle(n - 1);
+			System.out.println("Returning " + temp);
+			return temp;
+		}
 	}
 
 	public static String getString() throws IOException {
