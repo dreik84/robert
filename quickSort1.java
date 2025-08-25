810ed685ed12578d77d32d9de44e3b591ad0b649
dreik84 <dreik84@mail.ru>
2025-08-25 15:52:43 +0300

    Update quickSort1.java

--- quickSort1.java
+++ quickSort1.java
@@ -1,5 +1,22 @@
 // Простая версия быстрой сортировки
 
+class QuickSortApp {
+	
+	public static void main(String[] args) {
+		
+		int maxSize = 16;
+		ArrayIns arr = new ArrayIns(maxSize);
+		
+		for (int j = 0; j < maxSize; j++) {
+			long n = (int) (Math.random()*99);
+			arr.insert(n);
+		}
+
+		arr.display();
+		arr.quickSort();
+		arr.display();
+	}
+}
 
 class ArrayIns {
 	
@@ -28,10 +45,30 @@ class ArrayIns {
 			long pivot = theArray[right];
 			int partition = partitionIt(left, right, pivot);
 
-			recquicksort(left, partition - 1);
-			recquicksort(partition + 1, right);
+			recQuickSort(left, partition - 1);
+			recQuickSort(partition + 1, right);
 		}
 	}
+	
+	public int partitionIt(int left, int right, long pivot) {
+		int leftPtr = left - 1;
+		int rightPtr = right;
 
+		while (true) {
+			
+			while (theArray[++leftPtr] < pivot);
+			while (rightPtr > 0 && theArray[--rightPtr] > pivot);
 
+			if (leftPtr >= rightPtr) break;
+			else swap(leftPtr, rightPtr);
+		}
+		swap(leftPtr, right);
+		return leftPtr;
+	}
+
+	public void swap(int dex1, int dex2) {
+		long temp = theArray[dex1];
+		theArray[dex1] = theArray[dex2];
+		theArray[dex2] = temp;
+	}
 }
